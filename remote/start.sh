03f45fd98f032079b9e807308f7af059ba88e125
Tiziano <tizianocitro98@gmail.com>
2023-03-19 15:11:59 +0100

    Reorganized repo

--- remote/start.sh
+++ remote/start.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
 PLUGIN_NAME=cs-aware-connect
-CONTAINER_NAME=mm-ubuntu
-
-if [ -z "$1" ]
-  then
-    echo "Missing plugin name, using default $PLUGIN_NAME."
-  else
-    PLUGIN_NAME=$1
-fi
-
-if [ -z "$2" ]
-  then
-    echo "Missing container name, using default $CONTAINER_NAME."
-  else
-    CONTAINER_NAME=$2
-fi
+CONTAINER_NAME=cs-connect-base
 
 echo "Stopping containers if running."
 docker compose down
 echo "Containers stopped."
 
-DIR=../../build/plugins/$PLUGIN_NAME
-echo "Checking if the $DIR directory exists..."
+DIR=../config/plugins/$PLUGIN_NAME
+echo "Checking if the $DIR directory exist..."
 if [ -d "$DIR" ];
 then
     echo "$DIR directory exists. Removing directory..."
@@ -34,7 +20,7 @@ fi
 
 PLUGIN_DIR=csa-connect
 CONTAINER_PLUGIN_DIR=/home/$PLUGIN_DIR/dist/$PLUGIN_NAME
-HOST_PLUGIN_DIR=../../build/plugins/$PLUGIN_NAME
+HOST_PLUGIN_DIR=../config/plugins/$PLUGIN_NAME
 echo "Copying pluging from $CONTAINER_NAME:$CONTAINER_PLUGIN_DIR to $HOST_PLUGIN_DIR."
 docker cp $CONTAINER_NAME:$CONTAINER_PLUGIN_DIR $HOST_PLUGIN_DIR
 echo "Copy completed."
---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Stopping containers if running."
+docker compose down
+echo "Containers stopped."
+
+echo "Starting containers..."
+docker compose up -d
+echo "Containers started."
+
+echo "Cleaning up older volumes..."
+docker volume prune -f
+echo "Completed."
