aee64506969fd5b4892571fade029337e3a49727
Tiziano <tizianocitro98@gmail.com>
2023-04-16 13:38:06 +0200

    Added backup script

---
+++ remote/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USER=user
+HOST=host
+CONTAINER=cs-connect-base
+DIR=csa-connect
+
+# Execute tar command inside container to create archive
+ssh $USER@$HOST "docker exec $CONTAINER tar -czvf home/$DIR.tar.gz home/$DIR"
+
+# Copy the archive from the container to the remote machine
+ssh $USER@$HOST "docker cp $CONTAINER:/home/$DIR.tar.gz /home/$USER/backups/$DIR.tar.gz"
+
+# Copy the archive from the remote machine to the local machineP
+scp $USER@$HOST:/home/$USER/$DIR.tar.gz ./backups/$DIR.tar.gz
+
