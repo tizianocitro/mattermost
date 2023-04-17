a3231b05acb49335260318c8429dfa64ebdc15b8
Tiziano Citro <56075735+tizianocitro@users.noreply.github.com>
2023-04-17 10:54:25 +0200

    Typo in comment

--- remote/backup.sh
+++ remote/backup.sh
@@ -11,6 +11,6 @@ ssh $USER@$HOST "docker exec $CONTAINER tar -czvf home/$DIR.tar.gz home/$DIR"
 # Copy the archive from the container to the remote machine
 ssh $USER@$HOST "docker cp $CONTAINER:/home/$DIR.tar.gz /home/$USER/backups/$DIR.tar.gz"
 
-# Copy the archive from the remote machine to the local machineP
+# Copy the archive from the remote machine to the local machine
 scp $USER@$HOST:/home/$USER/$DIR.tar.gz ./backups/$DIR.tar.gz
 
